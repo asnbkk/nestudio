0825498d8537b9c5e570e4db04ee47eaddfca66f
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-11 21:11:48 +0600

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { SvgComponent } from './components/svg/svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectCardComponent,
+    SvgComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+  innerWidth
+  ballPosition
+  scrollPosition
+  @HostListener('window:scroll', ['$event']) 
+  doSomething() {
+    this.scrollPosition = window.pageYOffset
+    if(!this.isSmall()) {
+      this.ballPositionCounter(0)
+      //position absolute used
+    }
+    else {
+      this.ballPositionCounter(0)
+      //position absolute used
+    }
+  }
+
+  ballPositionCounter(padding) {
+    this.ballPosition = 1.25 * window.pageYOffset + padding * this.innerWidth / 100
+    let res = this.innerWidth - (padding * this.innerWidth / 100) - 136
+    if(this.ballPosition + 500 >= res) this.ballPosition = res - 500
+  }
+    
+  public arrayOfStrings = ['дизайн', 'стиль', 'творчество', 'креатив', 'смелость']
+  public charIndex = 0
+  public wordIndex = 0
+  public typedShit = ''
+  public typing: boolean = true
+  public pauseIndex = 0
+  
+  public erasing = false
+
+  type() {
+
+    setInterval(() => {
+      if (this.pauseIndex < 10) {
+        this.pauseIndex++
+        this.typing = false
+      }
+      else if (this.charIndex < this.arrayOfStrings[this.wordIndex].length && !this.erasing) {
+        this.typedShit += this.arrayOfStrings[this.wordIndex][this.charIndex]
+        this.charIndex++
+      } else {
+        this.erasing = true
+        return this.erase()
+      }
+    }, 100);
+  }
+
+  erase() {
+    if (this.pauseIndex < 50) {
+      this.pauseIndex++
+      this.typing = false
+    }
+    else if (this.charIndex > 0 && this.erasing) {
+      this.typedShit = this.typedShit.slice(0, -1);
+      this.charIndex--
+    } else {
+      this.pauseIndex = 0
+      this.typing = true
+      if (this.wordIndex >= this.arrayOfStrings.length - 1)
+        this.wordIndex = 0
+      else
+        this.wordIndex++
+      this.erasing = false
+      return 0
+    }
+  }
+
+  isSmall() {
+    if(this.innerWidth >= 1400) return false
+    return true
+  }
+  
+  ngOnInit(): void {
+    this.type()
+    this.innerWidth = window.innerWidth;
+  }
+}
