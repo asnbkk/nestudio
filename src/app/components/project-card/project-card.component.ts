385e4122c2ecb7c9d0d738bea19781bc5ad10b49
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-12 00:12:43 +0600

    hello

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProjectCardComponent implements OnInit {
   @Input() name
   @Input() text
-  focused: boolean = false
+  @Input() focused = false
+  // focused: boolean = false
   constructor() { }
 
   ngOnInit(): void {
