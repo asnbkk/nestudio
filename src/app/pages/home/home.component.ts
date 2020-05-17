f5425ad52d3f1f2060436f0cf416af36172cc3f7
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-18 00:11:37 +0600

    dog shit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  loader: boolean = false
+  // loader condition
   innerWidth
   ballPosition
   scrollPosition
-
+  
   aboutUsShuffle = [
     'HTML', 'App', 'Interface', 'CSS', 'Angular', 'SCSS', 'NodeJS', 
     'JavaScript', 'Responsive design', 'Semantic markup', 'SaaS', 'API', 
@@ -117,7 +120,18 @@ export class HomeComponent implements OnInit {
     this.type()
     this.shuffleArray(this.aboutUsShuffle)
     this.innerWidth = window.innerWidth;
+    // setTimeout(() => {
+    //   this.loader = false
+    // }, 0)
+    // for the future loader
   }
+//   x
+//   y
+//   captureCoordinate = function($event){
+//     this.x = $event.x;
+//     this.y = $event.y;
+//  }
+// for future mouse position
 
   scrollTo(text) {
     let projects = document.querySelector('.projects')
@@ -127,4 +141,25 @@ export class HomeComponent implements OnInit {
     if(text == 'О нас') aboutUs.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
     if(text == 'Контакты') contact.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
   }
+  currentView = 0
+  // onMouseWheel(evt) {
+  //   let main = document.querySelector('.home')
+  //   let projects = document.querySelector('.projects')
+  //   let aboutUs = document.querySelector('.au_title')
+  //   let contact = document.querySelector('.contact')
+
+  //   let views = [main, projects, aboutUs, contact]
+    
+  //   if(evt.deltaY > 0 && this.currentView <= views.length - 1 && this.currentView == 0) {
+  //       this.currentView = 1
+  //       projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+  //   }
+  //   else if(evt.deltaY < 0 && this.currentView >= 0) {
+  //       // this.currentView--
+  //       // views[this.currentView].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+  //   }
+  //   else console.log('jopa')
+
+  //   console.log(this.currentView)
+  // }
 }
