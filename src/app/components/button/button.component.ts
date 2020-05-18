9d5551a2978096441a7d09557e38c64478c252dc
BekkaliyevAssan <marina.neymar.13@gmail.com>
2020-05-18 19:17:12 +0600

    submit button animation

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -8,9 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text: string
   @Input() disabled: boolean
+  @Input() submit: boolean
+  clickSubmit: boolean = false
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    let temptext = this.text
+    this.clickSubmit = true
+    setTimeout(() => {
+      this.clickSubmit = false
+    }, 5000)
+    setTimeout(() => {
+      this.text = "Спасибо, заявка принята! Ожидайте ответа"
+    }, 1000)
+    setTimeout(() => {
+      this.text = temptext
+    }, 4000)
   }
 
 }
